13ce9433d7037066f6465e6b89a1cade22cf007a
Ezemba marvellous <90470256+marviigrey@users.noreply.github.com>
2023-09-10 16:17:48 +0100

    Update app.py

--- stage1/app.py
+++ stage1/app.py
@@ -1,36 +1,33 @@
-from flask import Flask, request, jsonify
+import json
 import datetime
-import os
 
-app = Flask(__name__)
-app.json.sort_keys=False
-
-@app.route('/api', methods=['GET'])
-def get_info():
-    slack_name = request.args.get('Ezemba Marvellous', default='Ezemba Marvellous')
-    track = request.args.get('Backend', default='Backend')
-
-    current_day = datetime.datetime.utcnow().strftime('%A')
-    current_utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%-d %H:%M:%S')
-
-    github_url_file = "https://github.com/marviigrey/HNG-BACKEND-INTERNSHIP/blob/master/stage1/app.py"
-    github_url_source = "https://github.com/marviigrey/HNG-BACKEND-INTERNSHIP"
-
-    github_url_file = 'https://github.com/marviigrey/HNG-BACKEND-INTERNSHIP/blob/master/stage1/app.py'
-    github_url_source = 'https://github.com/marviigrey/HNG-BACKEND-INTERNSHIP'
-
-    status_code = 200
-
+def lambda_handler(event, context):
+    # Extract query parameters from the event
+    slack_name = event.get('queryStringParameters', {}).get('slack_name', 'Ezemba Marvellous')
+    
+    # Get the current day of the week
+    current_day = datetime.datetime.now().strftime('%A')
+    
+    # Get the current UTC time
+    utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    
+    # Define other required information
+    track = "backend"
+    github_file_url = "https://github.com/marviigrey/HNG-BACKEND-INTERNSHIP/blob/master/stage1/app.py"
+    github_repo_url = "https://github.com/marviigrey/HNG-BACKEND-INTERNSHIP"
+    
+    # Prepare the JSON response
     response = {
-            'slack_name': slack_name,
-            'current_day': current_day,
-            'current_utc_time': current_utc_time,
-            'track': track,
-            'github_url_file': github_url_file,
-            'github_url_source': github_url_source,
-            'status_code': status_code
-
-            }
-    return jsonify(response)
-if __name__=='__main__':
-    app.run(host='0.0.0.0')
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": https://github.com/marviigrey/HNG-BACKEND-INTERNSHIP/blob/master/stage1/app.py,
+        "github_repo_url": https://github.com/marviigrey/HNG-BACKEND-INTERNSHIP,
+        "status_code": 200
+    }
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps(response)
+    }
